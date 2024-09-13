70742b6230a8b6664e4ece42029872caee336617
Seki <79982991+Danilocpm@users.noreply.github.com>
2024-09-13 18:08:55 -0300

    funcao ler mais e cards

--- books-app/src/app/book-search/book-search.component.ts
+++ books-app/src/app/book-search/book-search.component.ts
@@ -14,13 +14,38 @@ export class BookSearchComponent {
   books: any[] = [];
   query: string = '';
   filter: 'intitle' | 'inauthor' = 'intitle';
+  maxLength: number = 200; // Define o comprimento máximo da descrição
 
   constructor(private bookService: BookService) {}
 
   search(): void {
     this.bookService.searchBooks(this.query, this.filter).subscribe((data) => {
-      this.books = data.items;
+      this.books = data.items.map((book: any) => ({
+        ...book,
+        expanded: false  // Inicializa a propriedade expanded como false
+      }));
     });
   }
+
+  toggleExpand(book: any): void {
+    // Atualiza apenas a propriedade expanded do livro clicado
+    book.expanded = !book.expanded;
+  }
+
+  isLongDescription(description: string | undefined): boolean {
+    return (description && description.length > this.maxLength) ? true : false;
+  }
+
+  getDescription(description: string | undefined, expanded: boolean): string {
+    if (!description) return 'No description available';
+    if (expanded || description.length <= this.maxLength) {
+      return description;
+    } else {
+      return description.substring(0, this.maxLength) + '...';
+    }
+  }
 }
+
+
+
 
